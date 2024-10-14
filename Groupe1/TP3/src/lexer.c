096dc77e1cc39d8b61d2f70721871c7be7c48962
Remi Jara <remi.jara4@gmail.com>
2024-10-14 08:13:10 +0200

    Update on tp3

--- Groupe1/TP3/src/lexer.c
+++ Groupe1/TP3/src/lexer.c
@@ -5,43 +5,33 @@
 char **lexer(char *expression)
 {
     int expression_length = strlen(expression);
-    char **tokens = malloc(expression_length * sizeof(char *));
-    char *str = malloc(expression_length * sizeof(char));
-    str = strcpy(str, expression);
-    char *token = strtok(str, " ");
+    char **tokens = malloc((expression_length + 1) * sizeof(char *));
     int i = 0;
-    while (token != NULL)
-    {
-        // is float or int
-        if (atof(token) != 0 || token[0] == '0')
-        {
-            printf("float: %f\n", atof(token));
-            tokens[i] = token;
-        }
-        else
-        {
-            // is operator
-            printf("operator: %c\n", token[0]);
-            if (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/')
-            {
-                tokens[i] = token;
-            }
-        }
 
-        tokens[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    // check for syntax error (operator in first or last position)
-    if (tokens[i - 1][0] == '+' || tokens[i - 1][0] == '-' || tokens[i - 1][0] == '*' || tokens[i - 1][0] == '/')
+    for (int j = 0; j < expression_length; j++)
     {
-        printf("Syntax error: operator in last position\n");
-        return NULL;
+        if (expression[j] == ' ')
+        {
+            continue;
+        }
+        tokens[i] = malloc(2 * sizeof(char));
+        tokens[i][0] = expression[j];
+        tokens[i][1] = '\0';
+        i++;
     }
-    else if (tokens[0][0] == '+' || tokens[0][0] == '-' || tokens[0][0] == '*' || tokens[0][0] == '/')
+    tokens[i] = NULL;
+
+    // Check for syntax error (operator in first or last position)
+    if (tokens[0][0] == '+' || tokens[0][0] == '-' || tokens[0][0] == '*' || tokens[0][0] == '/')
     {
         printf("Syntax error: operator in first position\n");
         return NULL;
     }
+    else if (tokens[i - 1][0] == '+' || tokens[i - 1][0] == '-' || tokens[i - 1][0] == '*' || tokens[i - 1][0] == '/')
+    {
+        printf("Syntax error: operator in last position\n");
+        return NULL;
+    }
+
     return tokens;
 }
